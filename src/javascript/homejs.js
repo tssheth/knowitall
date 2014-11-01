d91f88b461bc0e48926d7064e2343fd815b7a10a
UltimatePanda231 <ankurgupta@Ankur-Guptas-MacBook-Pro-4.local>
2014-11-01 12:55:58 -0400

    contiuned working on the rss reader

--- src/javascript/homejs.js
+++ src/javascript/homejs.js
@@ -2,4 +2,31 @@
  * YHack 2014
  */
 
+function myGetElementsByClassName(selector) {
+    if (document.getElementsByClassName ) {
+        return document.getElementsByClassName(selector);
+    }
+    
+    var returnList = new Array();
+    var nodes = document.getElementsByTagName('div');
+    var max = nodes.length;
+    for (var i = 0; i < max; i++) {
+        if(nodes[i].className == selector) {
+            returnList[returnList.length] = nodes[i];
+        }
+    }
+    return returnList;
+}
 
+var rssReader = {
+    containers: NULL,
+    
+    init : function(selector) {
+        containers = myGetElementsByClassName(selector);
+        for(i = 0; i < containers; i++) {
+            var rssUrl = containers[i].getAttribute('rssUrl');
+            var rssNum = containers[i].getAttribute('rssNum');
+            var id = containers[i].getAttribute('id');
+        }
+    }
+}
