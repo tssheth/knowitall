56ca1f1f63955bad6bf7ff80404eb8a832bf92f3
agupta231 <rukna1000@gmail.com>
2014-11-16 03:00:41 -0500

    working on user data management

--- src/javascript/resize.js
+++ src/javascript/resize.js
@@ -2,63 +2,65 @@ var results = document.getElementsByClassName('post_results');
 
 function resizeY() {
     var height = window.innerHeight - 188;
-    for(var k = 0; k < results.length; k++) {
-        results[k].style.height = height+'px';
-    }    
-}
-
-function resizeX() {
-    var container = document.getElementById('container');
-    var width = window.innerWidth;
-    var listItems = document.getElementsByTagName('li');
+    var counter = 0;
     
-    if(width < 1165) {
-        for(var i = 0; i < listItems.length; i++) {
-            listItems[i].style.height = '85px';
-            
-            results[i].style.width = '280px';
-            results[i].style.height = '350px';
+    for(var i = 0; i < results.length; i++) {
+        if(results[i].style.display !== "none") {
+            counter++;
         }
     }
-    if(width >= 1165) {
-       for(var i = 0; i < listItems.length; i++) {
-            listItems[i].style.height = '60px';
-            
-            results[i].style.width = '350px';
-        } 
-    }
+    
+//    if(counter < 4) {
+        for(var k = 0; k < results.length; k++) {
+            results[k].style.height = height+'px';
+        }  
+//    }
+//    if(counter => 4) {
+//        for(var j = 0; j < results.length; j++) {
+//            results[j].style.height = (height)/2+'px';
+//        }
+//    }
+//    counter = 0;
 }
 
-//function resizeMaster() {
-//    var counter = 0;
+//function resizeX() {
+//    var container = document.getElementById('container');
+//    var width = window.innerWidth;
+//    var listItems = document.getElementsByTagName('li');
 //    
-//    if(document.getElementById("newsSub").style.display !== "none") {
-//        counter++;
-//    }
-//    if(document.getElementById("sportssSub").style.display !== "none") {
-//        counter++;
-//    }
-//    if(document.getElementById("marketSub").style.display !== "none") {
-//        counter++;
-//    }
-//    
-//    var subs = document.getElementsByClassName("subcontainer");
-//    
-//    for(var j = 0; j < subs.length; j++) {
-//        if(subs[j].style.display !== "none") {
-//            counter++;
+//    if(width < 1165) {
+//        for(var i = 0; i < listItems.length; i++) {
+//            listItems[i].style.height = '85px';
+//            
+//            results[i].style.width = '280px';
+//            results[i].style.height = '350px';
 //        }
 //    }
-//    if(counter > 3) {
-//        for(var l = 0; l < subs.length; l++) {
-//            var height = (window.innerHeight - 188) / 2;
-//            subs[l].style.height = height+'px';
-//        }
+//    if(width >= 1165) {
+//       for(var i = 0; i < listItems.length; i++) {
+//            listItems[i].style.height = '60px';
+//            
+//            results[i].style.width = '350px';
+//        } 
 //    }
 //}
 
+function resizeMaster() {
+    var width = window.innerWidth;
+    var numberOfResults = results.length;
+    if(numberOfResults > 4) {
+        numberOfResults = 4;
+    }
+    else {
+        netWidth = width - (80*numberOfResults);
+        for(var i = 0; i < numberOfResults; i++) {
+            results[i].style.width = netWidth / numberOfResults;
+        }
+    }
+}
+
 function resize() {
     resizeY();
-    resizeX();
-//    resizeMaster();
+//    resizeX();
+    resizeMaster();
 }
